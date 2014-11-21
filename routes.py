897405b74f056a01ab058bc49dffe6477f430846
Larissa Muramoto <larissa.muramoto@gmail.com>
2014-11-20 17:21:34 -0800

    Making frontend pretty and building out user accounts

--- routes.py
+++ routes.py
@@ -9,10 +9,21 @@ import model
 import io
 
 import os
+
+#import base64
+
+from base64 import decodestring
+
+from binascii import a2b_base64
+
+from PIL import Image
+
 from flask import Flask, request, redirect, url_for
 from werkzeug import secure_filename
 
-UPLOAD_FOLDER = '/Users/Larissa/Desktop/testupload'
+from datetime import datetime
+
+UPLOAD_FOLDER = '/Users/Larissa/pika/app/static/images/purikuras/'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 
@@ -22,38 +33,6 @@ app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT' #why do I have this again..?
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 #editor = Blueprint('editor', __name__, template_folder='static')
-
-#testing some data stuff. please ignore
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-
-@app.route('/test', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return render_template('displaytest.html', filename=filename)
-            #return redirect(url_for('displaytest',
-            #                        filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
-
-# @app.route('/displaytest')
-# def displaytest():
-# 	return """
-# 	<p>%s</p>
-
-# 	""" % filename
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -95,10 +74,15 @@ def login():
 			flash("Incorrect username or password. Please try again!")
 			return redirect(url_for("login"))
 
+
+		#DOES THIS WORK?!?!
+		session['id'] = user.id
+		print "user id is: ", session['id']
+
+
 		session['email'] = email
 		session['logged_in'] = True
 		return redirect(url_for("main"))
-
 	return render_template("login.html")
 
 @app.route('/logout')
@@ -110,14 +94,15 @@ def logout():
 @app.route('/main')
 def main():
 	#get purikuras from the database
-	purikuras = []
+	purikuras = model.session.query(model.Purikura).filter_by(user_id=session['id'])
 
 	return render_template('main.html', purikura_list = purikuras)
 
-#just for testing. not getting purikura info from server. just displaying hardcoded image
-@app.route('/purikuradetailstest')
+#just for testing. getting image from server.
+@app.route('/purikuradetails')
 def show_purikura():
-	return render_template('purikura_details.html')
+	filename = request.args.get('filename')
+	return render_template('purikura_details.html', filename=filename)
 
 # @app.route('/purikura/<int:id>')
 # def show_purikura(id):
@@ -135,42 +120,113 @@ def choose():
 		pass
 	return render_template("choose.html")
 
+# #testing some data stuff. please ignore
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/uploadfile', methods=['POST'])
+def upload_file():
+	print "HAIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII"
+	
+	file = request.files['file']
+
+	if file and allowed_file(file.filename):
+		#do the image stuff
+		#file = enhance_image(file)
+		#save to disk
+		filename = secure_filename(file.filename)
+
+		#modify the filename so every purikura is unique
+		filename, extension = os.path.splitext(filename)
+		filename = str(session['id']) + filename + str(datetime.utcnow()).replace(" ", "") + extension
+
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+		full_filename = "static/images/purikuras/" + filename
+		print "full_filename: ", full_filename
+
+
+        #save file path to database
+
+        newPurikura = model.Purikura(user_id=session['id'], combined_src=full_filename, foreground_src=full_filename)
+        model.session.add(newPurikura)
+        model.session.commit()
+	
+        
+        return redirect(url_for('decorate', filename=full_filename))
+	# 	#return redirect(url_for('show_purikura', filename=full_filename))
+ 
+def enhance_image(raw_file):
+	#do image processing stuff
+	return raw_file
 
 @app.route('/photobooth')
 def photobooth():
 	return render_template('photobooth.html')
 
+#NEED A FUNCTION TO CONVERT BASE64 TO FILE.
+@app.route('/uploadwebcamphoto', methods=['POST'])
+def upload_webcam_photo():
+	raw_file = request.form['selectedPhotos']
+
+	storage_filename = 'static/images/purikuras/testphotoboothimage.jpeg'
+	convert_datauri_to_file(raw_file, storage_filename)
+
+	#factor this out later
+	newPurikura = model.Purikura(user_id=session['id'], combined_src=storage_filename, foreground_src=storage_filename)
+	model.session.add(newPurikura)
+	model.session.commit()
+
+	return redirect(url_for('decorate', filename=storage_filename))
+
+
+def convert_datauri_to_file(datauri, filename):
+	b64_data = datauri.replace("data:image/jpeg;base64,", "").strip()
+
+	binary_data = a2b_base64(b64_data)
+
+	fd = open(filename, 'wb')
+	fd.write(binary_data)
+	fd.close()
+
 @app.route('/about')
 def about():
 	return render_template('about.html')
 
-@app.route('/processimage', methods=['POST'])
-def process_image():
-	if request.method == 'POST':
-		snapshots = request.form.getlist("selectedPhotos")
-		# session['snapshots'] = snapshots
-		print "snapshots:", snapshots
-
-		#return HttpResponse(pic, content_type='image/jpeg')
-
-		#convert to openCV
-		"""
-		openCVimage = numpy.array(im)
-		openCVimage = openCVimage[:, :, ::-1].copy()
-
-
-		#process image using openCV
-		"""
-
-		return redirect("/decorate")
-		
-	# elif request.method == 'GET':
-	# 	return render_template('decorate.html')
 
 @app.route('/decorate', methods=['GET'])
 def decorate():
 	#return render_template('decorate.html', snapshots=session['snapshots'])
-	return render_template('decorate.html')
+	return render_template('decorate.html', filename=request.args.get('filename'))
+
+# @app.route('/decorateold', methods=['GET'])
+# def decorate_old():
+# 	#return render_template('decorate.html', snapshots=session['snapshots'])
+# 	return render_template('decorate_prestyle.html', filename=request.args.get('filename'))
+
+@app.route('/deletepurikura', methods=['POST'])
+def delete_purikura():
+	purikura = model.session.query(model.Purikura).filter_by(user_id=session['id'], combined_src=request.form['filename']).delete()
+	model.session.commit()
+	return redirect(url_for('main'))
+
+
+@app.route('/updatepurikura')
+def update_purikura():
+	#the frontend will send you the filename. you should know the user id.
+	#query for the purikura
+	#change some stuff
+	#put the revised stuff back in the database
+	#redirect to main?
+	
+
+	
+	
+	purikura = model.session.query(model.Purikura).filter_by(user_id=session['id'], combined_src=request.form['filename'])
+	purikura.combined_src = "" #some new thing you pass in?
+	model.session.commit()
+	return json.dumps({'status':'OK','user':user,'pass':password})
 
 if __name__ == '__main__':
     # debug=True gives us error messages in the browser and also "reloads" our web app
